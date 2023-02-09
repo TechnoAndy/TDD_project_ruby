0dafa3061d6c719a873f73c4952a72790ff3e528
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-09 13:48:13 +0200

    Update solver_spec.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -16,12 +16,12 @@ describe Solver do
       expect(@solver.factorial(2)).to eq(2)
     end
 
-    it 'Return an error on a negative number' do
+    it 'Return an error if a negative number is used' do
       expect { @solver.factorial(-1) }.to(raise_error(StandardError))
     end
   end
 
-  describe 'Reverse' do
+  describe 'Reverse word' do
     it 'returns the reverse of a word' do
       expect(@solver.reverse('hello')).to eq('olleh')
     end
@@ -29,18 +29,18 @@ describe Solver do
 
   describe 'fizzbuzz' do
     it 'should return fizzbuzz when number is divisible by 3 and 5' do
-      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(@solver.fizzbuzz(30)).to eq('fizzbuzz')
     end
 
-    it 'should return fizz if number is divisible by 3' do
-      expect(@solver.fizzbuzz(9)).to eq('fizz')
+    it 'if number is divisible by 3 return fizz' do
+      expect(@solver.fizzbuzz(12)).to eq('fizz')
     end
 
-    it 'should return Buzz if number is  divisible by 5' do
-      expect(@solver.fizzbuzz(5)).to eq('buzz')
+    it 'if number is divisible by 5 return buzz' do
+      expect(@solver.fizzbuzz(10)).to eq('buzz')
     end
 
-    it 'should return number if number is not divisible by 3 or 5' do
+    it 'if number is not divisible by 3 or 5 return number as a string' do
       expect(@solver.fizzbuzz(7)).to eq('7')
     end
   end
