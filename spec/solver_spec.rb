930996fafd785ee168171fb5e5b689ab6119e4c8
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-09 13:30:48 +0200

    Add code and run unit test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,15 +9,15 @@ describe Solver do
 
   describe 'Factorial' do
     it 'Return 1 for factorial of 0' do
-      expect(@solver.factorial(0).to(eq(1)))
+      expect(@solver.factorial(0)).to eq(1)
     end
 
     it 'Return factorial' do
-      expect(@solver.factorial(2).to(eq(2)))
+      expect(@solver.factorial(2)).to eq(2)
     end
 
     it 'Return an error on a negative number' do
-      expect(@solver.factorial(-1).to(raise_error(StandardError)))
+      expect { @solver.factorial(-1) }.to(raise_error(StandardError))
     end
   end
 
